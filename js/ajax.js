01509035b27ba1eba1bde55c78a02b9c10426b6e
Esteban Casalotti <estebancasalotti2@gmail.com>
2021-09-17 22:51:02 -0300

    ajax

---
+++ js/ajax.js
@@ -0,0 +1,20 @@
+  
+const url = "https://jsonplaceholder.typicode.com/posts"
+$.get(url, (repuesta, estado) => {
+    if(estado === "success") {
+        console.log(repuesta);
+        repuesta.forEach(e => {
+            $("#news").append(`
+            <p>${e.id}</p>
+            <p>${e.title}</p>
+            
+            `)
+            
+        });
+
+
+
+
+
+     
+}})
---
+++ js/theme.js
@@ -0,0 +1,13 @@
+$("body").prepend('<div id = "titulo">Los Ceibos</div>');
+$("#titulo").css({
+    "width": '100%',
+    "overflow": 'hidden',
+    "background-color": '#252932',
+    "margin-bottom": '20px',
+    "color": '#f2f2f2',
+    "float": 'left',
+    "line-height": '100px',
+    "font-size": '50px',
+})
+    .slideUp(500)
+    .slideDown(500)
